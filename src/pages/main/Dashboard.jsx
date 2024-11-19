aa8a356c62f214faf40aa973ea94023f574c6bff
eaint <eaintkkhinekyaw@gmail.com>
2024-11-19 13:36:09 +0700

    add search feature to dashboard

--- src/pages/main/Dashboard.jsx
+++ src/pages/main/Dashboard.jsx
@@ -1,20 +1,76 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Topic from '../../components/Topic'
 import styles from "./Dashboard.module.css"
 import java from "../../assets/demo/java.svg"
+import searchIcon from "../../assets/searchIcon.svg"
 
 export default function Dashboard() {
+
+  // Topics Data
+  const topics = [
+    { img: java, tas: 0, books: 0, title: 'Data Structure and Algorithms' },
+    { img: java, tas: 10, books: 10, title: 'Java' },
+    { img: java, tas: 10, books: 10, title: 'Python' },
+    { img: java, tas: 10, books: 10, title: 'Rust' },
+    { img: java, tas: 10, books: 10, title: 'General Talk' },
+    { img: java, tas: 10, books: 10, title: 'General Talk' },
+
+
+  ];
+
+  const [search,setSearch] = useState('');
+  const [filterTopics, setFilterTopics] = useState(topics);
+
+  const handleSearch = (e) => {
+    const query = e.target.value.toLowerCase();
+    setSearch(query);
+
+    const filtered = topics.filter(topic => topic.title.toLowerCase().includes(query));
+    setFilterTopics(filtered);
+  }
+
+  const handleSearchIconClick = () => {
+    const query = search.toLowerCase();
+    setSearch(query);
+
+    const filtered = topics.filter(topic => topic.title.toLowerCase().includes(query));
+    setFilterTopics(filtered);
+    console.log("search triggered", filtered)
+  }
+
   return (
-    <div className={`${styles.dashboard}`}>
-      <div className={`${styles.container}`}>
-        <Topic img={java} tas={0} books={0} title='Data Structure and Algorithm' />
-        <Topic img={java} tas={0} books={0} title='Java' />
-        <Topic img={java} tas={0} books={0} title='Data Structure and Algorithm' />
-        <Topic img={java} tas={0} books={0} title='Java' />
-        <Topic img={java} tas={0} books={0} title='Data Structure and Algorithm' />
-        <Topic img={java} tas={0} books={0} title='Java' />
+    <>
+      <p className={`${styles.title}`}>Find Your Subject, Book a TA and Achieve Success!</p>
+      <div className={`${styles.searchContainer}`}>
+        <div className={`${styles.searchInnerWrapper}`}>
+          <input
+              type="text"
+              placeholder="eg. Data Structure"
+              value={search}
+              onChange={handleSearch}
+              className={`${styles.inputContent}`}
+            />
+            <img src={searchIcon} alt="Search Icon" onClick={handleSearchIconClick} className={`${styles.searchIcon}`}/>
+          </div>
       </div>
-      <br /><br />
-    </div>
+
+      <div className={`${styles.dashboard}`}>
+      {/* Topics List */}
+        <div className={`${styles.container}`}>
+          {filterTopics.map((topic, index) => (
+            <Topic
+              key={index}
+              img={topic.img}
+              tas={topic.tas}
+              books={topic.books}
+              title={topic.title}
+            />
+          ))}
+        </div>
+        <br /><br />
+      </div>
+    </>
+
+
   )
 }
